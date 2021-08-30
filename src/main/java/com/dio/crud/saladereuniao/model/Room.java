8a0d51a523082daf7f0e62fab558e1e3edf77455
dsouzarogerio <dsouza.rogerio@gmail.com>
2021-08-30 11:36:12 -0300

    inicial

---
+++ src/main/java/com/dio/crud/saladereuniao/model/Room.java
@@ -0,0 +1,125 @@
+package com.dio.crud.saladereuniao.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Room {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;	
+	
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private String date;
+	
+	@Column(name = "start_hour", nullable = false)
+	private String startHour;
+	
+	@Column(name = "end_hour", nullable = false)
+	private String endHour;
+	
+	public Room() {
+		
+	}
+	
+	public Room(Long id, String name, String date, String startHour, String endHour) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.date = date;
+		this.startHour = startHour;
+		this.endHour = endHour;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getStartHour() {
+		return startHour;
+	}
+	public void setStartHour(String startHour) {
+		this.startHour = startHour;
+	}
+	public String getEndHour() {
+		return endHour;
+	}
+	public void setEndHour(String endHour) {
+		this.endHour = endHour;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + ((endHour == null) ? 0 : endHour.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((startHour == null) ? 0 : startHour.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Room other = (Room) obj;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (endHour == null) {
+			if (other.endHour != null)
+				return false;
+		} else if (!endHour.equals(other.endHour))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (startHour == null) {
+			if (other.startHour != null)
+				return false;
+		} else if (!startHour.equals(other.startHour))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Room [id=" + id + ", name=" + name + ", date=" + date + ", startHour=" + startHour + ", endHour="
+				+ endHour + "]";
+	}
+
+
+}
